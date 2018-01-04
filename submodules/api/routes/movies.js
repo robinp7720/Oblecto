5b0e99a102937549222d40e8300f2d22423664e5
Robin <robin@decker.cx>
2018-01-04 17:02:07 +0100

    Added movie routes and artwork downloading

--- submodules/api/routes/movies.js
+++ submodules/api/routes/movies.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
+import path from 'path';
+import fs from 'fs';
+
 import config from "../../../config";
 import databases from "../../../submodules/database";
+import tvdb from "../../tvdb";
 
 export default (server) => {
 
@@ -15,5 +19,56 @@ export default (server) => {
             next();
         });
     };
+
+    // Endpoint to get a list of episodes from all series
+    server.get('/movies/list/:sorting/:order', requiresAuth, function (req, res, next) {
+        databases.movie.findAll({
+            order: [
+                [req.params.sorting, req.params.order]
+            ],
+            limit: 30
+        })
+            .then((results) => res.send(results));
+    });
+
+
+
+    // Endpoint to get a poster based on localId
+    server.get('/movie/:id/poster', function (req, res, next) {
+        // Get episode data
+        databases.movie.findById(req.params.id, {include: [databases.file]}).then(movie => {
+            let moviePath = movie.files[0].path;
+
+            // Set the thumbnail to have the same name but with -thumb.jpg instead of the video file extension
+            let posterPath = moviePath.replace(path.extname(moviePath), "-poster.jpg");
+
+            // Check if the thumbnail exists
+            fs.exists(posterPath, function (exists) {
+                if (exists) {
+                    // If the thumbnail exists, simply pipe that to the client
+                    fs.createReadStream(posterPath).pipe(res)
+                }
+            });
+        });
+    });
+
+    // Endpoint to get a fanart based on localId
+    server.get('/movie/:id/fanart', function (req, res, next) {
+        // Get episode data
+        databases.movie.findById(req.params.id, {include: [databases.file]}).then(movie => {
+            let moviePath = movie.files[0].path;
+
+            // Set the thumbnail to have the same name but with -thumb.jpg instead of the video file extension
+            let fanartPath = moviePath.replace(path.extname(moviePath), "-fanart.jpg");
+
+            // Check if the thumbnail exists
+            fs.exists(fanartPath, function (exists) {
+                if (exists) {
+                    // If the thumbnail exists, simply pipe that to the client
+                    fs.createReadStream(fanartpath).pipe(res)
+                }
+            });
+        });
+    });
 
 };
