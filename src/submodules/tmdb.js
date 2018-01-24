b707909f16f43d0676589604448dc9229fc2d6fa
Robin <robin@decker.cx>
2018-01-24 22:52:27 +0100

    Fixes movieDB api

--- src/submodules/tmdb.js
+++ src/submodules/tmdb.js
@@ -1,4 +1,7 @@
 import config from "../config.json";
 import moviedb from "moviedb-promise";
-export default moviedb(config.themoviedb.key);
+
+const moviedbInstance = new moviedb(config.themoviedb.key);
+
+export default moviedbInstance;
 
