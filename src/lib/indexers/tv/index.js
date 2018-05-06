f6d7faca3841f3915b183ba918c18d89d72945f1
Robin <robin@decker.cx>
2018-05-06 13:59:58 +0200

    Added extra logging

--- src/lib/indexers/tv/index.js
+++ src/lib/indexers/tv/index.js
@@ -4,9 +4,11 @@ import config from "../../../config";
 
 export default {
     async indexDirectory(Directory) {
+        console.log("Indexing", Directory);
         let files = await recursive(Directory);
 
         files.forEach(file => {
+            console.log("Pushing file", file, "to queue");
             queue.push({task: "episode", path: file}, function (err) {
 
             });
