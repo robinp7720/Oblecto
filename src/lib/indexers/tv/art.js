c3613172cff60ccfb4bea4515f349ec4840f2ad9
Robin <robin@decker.cx>
2018-05-06 13:58:14 +0200

    Re-download image if image size is to small

--- src/lib/indexers/tv/art.js
+++ src/lib/indexers/tv/art.js
@@ -6,64 +6,89 @@ import fs from "fs";
 import request from "request"
 
 export default {
+    imageExists(imagePath) {
+        // Assume the image exists at first
+        let imageExists = true;
+
+        try {
+            let stat = fs.statSync(imagePath);
+
+            // Re-download thumbnail if it's to small in size
+            // This may mean that the thumbnail image is corrupt or wasn't downloaded properly the first time.
+            if (stat.size < 1000) {
+                // Delete the thumbnail
+                fs.unlinkSync(imagePath);
+
+                console.log("Image exists for", imagePath, "but will be re-downloaded");
+                throw "poster to small"
+            }
+
+            console.log("Image exists for", imagePath);
+        } catch (e) {
+            // If an error has occurred above, it's because either the file stating threw an error or because the poster
+            // image was to small.
+            imageExists = false
+        }
+
+        return imageExists;
+    },
+
+
     async DownloadEpisodeBanner(id) {
         let episode = await databases.episode.findById(id, {include: [databases.file]});
 
         let episodePath = episode.files[0].path;
 
-        console.log("Checking thumnail for", episodePath);
+        console.log("Checking thumbnail for", episodePath);
 
         // Set the thumbnail to have the same name but with -thumb.jpg instead of the video file extension
         let thumbnailPath = episodePath.replace(path.extname(episodePath), "-thumb.jpg");
 
+        if (this. imageExists(thumbnailPath))
+            return;
 
-        try {
-            let stat = fs.statSync(thumbnailPath);
+        // If no thumbnail was found, download one from thetvdb
+        let data = await tvdb.getEpisodeById(episode.tvdbid);
 
-            console.log("Thumnail exists for", episodePath);
-        } catch (e) {
-            // If no thumbnail was found, download one from thetvdb
-            let data = await tvdb.getEpisodeById(episode.tvdbid);
+        request.get({
+            uri: "https://thetvdb.com/banners/_cache/" + data.filename,
+            encoding: null
+        }, function (err, response, body) {
+            fs.writeFile(thumbnailPath, body, function (error) {
+                if (error) {
+                    console.error("An error has occured when downloading banner for", episodePath);
+                }
 
-            request.get({
-                uri: "https://thetvdb.com/banners/_cache/" + data.filename,
-                encoding: null
-            }, function (err, response, body) {
-                fs.writeFile(thumbnailPath, body, function (error) {
-                    if (error) {
-                        console.error("An error has occured when downloading banner for", episodePath);
-                    }
-
-                    console.log("Image downloaded for", episodePath);
-                });
-            })
-        }
+                console.log("Image downloaded for", episodePath);
+            });
+        })
     },
 
     async DownloadSeriesPoster(id) {
         let show = await databases.tvshow.findById(id);
 
         let showPath = show.directory;
+
         let posterPath = path.join(showPath, show.seriesName + '-poster.jpg');
 
-        // Check if the poster image already exits
-        let exists = fs.existsSync(posterPath);
+
+        if (this.imageExists(posterPath))
+            return;
 
         // Only download the poster if it doesn't exist
-        if (!exists) {
-            let data = await tvdb.getSeriesPosters(show.tvdbid);
 
-            console.log("Downloading poster image for", show.seriesName, "http://thetvdb.com/banners/" + data[0].fileName);
-            request.get({
-                uri: "http://thetvdb.com/banners/" + data[0].fileName,
-                encoding: null
-            }, function (err, response, body) {
-                fs.writeFile(posterPath, body, function (error) {
-                    if (!error)
-                        console.log("Poster downloaded for", show.seriesName);
-                });
-            })
-        }
+        let data = await tvdb.getSeriesPosters(show.tvdbid);
+
+        console.log("Downloading poster image for", show.seriesName, "http://thetvdb.com/banners/" + data[0].fileName);
+        request.get({
+            uri: "http://thetvdb.com/banners/" + data[0].fileName,
+            encoding: null
+        }, function (err, response, body) {
+            fs.writeFile(posterPath, body, function (error) {
+                if (!error)
+                    console.log("Poster downloaded for", show.seriesName);
+            });
+        })
     },
 
     /**
