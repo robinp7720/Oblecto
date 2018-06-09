17f10a26a6ef5e5e24931b065a8a6db6cedd47e6
Robin <robin@decker.cx>
2018-06-09 16:50:20 +0200

    redirect play routes to the streaming route

--- src/submodules/REST/routes/episodes.js
+++ src/submodules/REST/routes/episodes.js
@@ -77,53 +77,15 @@ export default (server) => {
 
     // Endpoint to send episode video file to the client
     // TODO: move this to the file route and use file id to play, abstracting this from episodes
-    server.get('/episode/:id/play', async function (req, res) {
+    server.get('/episode/:id/play', async function (req, res, next) {
         // search for attributes
         let episode = await databases.episode.findById(req.params.id, {
             include: [databases.file]
         });
 
-        let path = episode.files[0].path;
-        var stat = fs.statSync(path);
-        var total = stat.size;
+        let file = episode.files[0];
 
-        if (req.headers.range) { // meaning client (browser) has moved the forward/back slider
-            // which has sent this request back to this server logic ... cool
-            var range = req.headers.range;
-            var parts = range.replace(/bytes=/, '').split('-');
-            var partialstart = parts[0];
-            var partialend = parts[1];
-
-            var start = parseInt(partialstart, 10);
-            var end = partialend ? parseInt(partialend, 10) : total - 1;
-            var chunksize = (end - start) + 1;
-            console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-
-            var file = fs.createReadStream(path, {
-                start: start,
-                end: end
-            });
-
-            res.writeHead(206, {
-                'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunksize,
-                'Content-Type': 'video/mp4'
-            });
-
-            file.pipe(res);
-
-        } else {
-
-            console.log('ALL: ' + total);
-
-            res.writeHead(200, {
-                'Content-Length': total,
-                'Content-Type': 'video/mp4'
-            });
-            
-            fs.createReadStream(path).pipe(res);
-        }
+        res.redirect(`/stream/${file.id}`, next);
 
     });
 
