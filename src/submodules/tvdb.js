c78ce781e4e61d0dbaba6c03202c6ddc04d3acb1
Robin <robin@decker.cx>
2018-06-09 17:54:42 +0200

    Remove unused then

--- src/submodules/tvdb.js
+++ src/submodules/tvdb.js
@@ -6,10 +6,7 @@ let tvdb = new TVDB(config.tvdb.key);
 // Auto reconnect to tvdb if connection has died
 setInterval(function () {
     tvdb.getSeriesById(70851)
-        .then(function (showData) {
-
-        })
-        .catch(function (err) {
+        .catch(function () {
             tvdb = new TVDB(config.tvdb.key);
         });
 }, 1000 * 60 * 60);
