c631cdce6829ec1c149a356a4e0279baaaa019b1
Robin <robin@decker.cx>
2018-12-11 23:55:24 +0100

    Send index.html under every route under /web expect for the static sub dir

--- src/submodules/REST/routes/web.js
+++ src/submodules/REST/routes/web.js
@@ -1,13 +1,20 @@
-import jwt from 'jsonwebtoken';
-import errors from 'restify-errors';
-
-import databases from '../../../submodules/database';
-import config from '../../../config';
 import restify from 'restify'
+import fs from "fs"
 
 export default (server) => {
-    server.get('/web/*', restify.plugins.serveStatic({
-        directory: __dirname + '../../../../../Oblecto-Web/dist/',
-        default: 'index.html'
+    server.get('/web/static/*', restify.plugins.serveStatic({
+        directory: __dirname + '/../../../../Oblecto-Web/dist/',
     }));
+
+    server.get('/web*', async (req, res, next) => {
+        fs.readFile(__dirname + '/../../../../Oblecto-Web/dist/web/index.html', 'utf8', function (err, body) {
+            res.writeHead(200, {
+                'Content-Length': Buffer.byteLength(body),
+                'Content-Type': 'text/html'
+            });
+
+            res.write(body);
+            res.end();
+        });
+    });
 };
