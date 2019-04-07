089d76d3aaa786452fb81beb1fcd282f822f6cfa
Robin <robin@decker.cx>
2019-04-07 15:23:43 +0200

    Host logomark at /web/logo.png

--- src/submodules/REST/routes/web.js
+++ src/submodules/REST/routes/web.js
@@ -6,6 +6,19 @@ export default (server) => {
         directory: __dirname + '/../../../../Oblecto-Web/dist/',
     }));
 
+
+    server.get('/web/logo.png', async (req, res, next) => {
+        fs.readFile(__dirname + '/../../../../images/logomark.png', function (err, body) {
+            res.writeHead(200, {
+                'Content-Length': Buffer.byteLength(body),
+                'Content-Type': 'image/png'
+            });
+
+            res.write(body);
+            res.end();
+        });
+    });
+
     server.get('/web*', async (req, res, next) => {
         fs.readFile(__dirname + '/../../../../Oblecto-Web/dist/web/index.html', 'utf8', function (err, body) {
             res.writeHead(200, {
