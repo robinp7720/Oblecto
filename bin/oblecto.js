b11dabcf1634f1a914be385a82b235841e66b4c4
Robin <robin@decker.cx>
2019-04-13 14:10:06 +0200

    Add preliminary CLI interface to oblecto start script

--- bin/oblecto.js
+++ bin/oblecto.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
-require("@babel/polyfill");
-require('../dist/index.js');
+require('@babel/polyfill');
+
+var oblectoPackage = require(__dirname + '/../package.json');
+
+var args = process.argv.slice(2);
+
+switch (args[0]) {
+    case 'start':
+        require('../dist/index.js');
+        break;
+    case 'adduser':
+        break;
+    case 'deluser':
+        break;
+    case 'index':
+        break;
+    case 'downloadart':
+        break;
+    default:
+        console.log(`Oblecto version ${oblectoPackage.version}`);
+        console.log()
+        console.log(`Usage:`);
+        console.log(`oblecto start`);
+        console.log(`oblecto adduser USERNAME PASSWORD REALNAME EMAIL`);
+        console.log(`oblecto deluser USERNAME`);
+        console.log(`oblecto index [all|tvshows|movies]`);
+        console.log(`oblecto downloadart [all|tvshows|movies]`);
+
+}
