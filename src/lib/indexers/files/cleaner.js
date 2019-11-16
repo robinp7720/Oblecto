9e69852829235f7f0eef9029508a38e39d4ba26a
Robin <robin@decker.cx>
2019-11-17 00:33:59 +0100

    Add console output for cleaner

--- src/lib/indexers/files/cleaner.js
+++ src/lib/indexers/files/cleaner.js
@@ -3,6 +3,7 @@ import databases from '../../../submodules/database';
 
 export default {
     async removedDeletedFiled () {
+        console.log('Scanning for deleted files');
         let results = await databases.file.findAll();
 
         results.forEach((item) => {
@@ -17,12 +18,15 @@ export default {
 
     },
     async removeAssoclessFiles () {
+        console.log('Scanning for files without associated media entities');
+
         let results = await databases.file.findAll({
             include: [databases.movie, databases.episode]
         });
 
         results.forEach((item) => {
             if (item.movies.length === 0 && item.episodes.length === 0) {
+                console.log('Deleting', item.path, 'because the file doesn\'t have any media entities associated')
                 item.destroy();
             }
         });
