08ce997463d4388ec966050573dcfd58015ac372
Robin <robin@decker.cx>
2020-07-15 12:16:36 +0200

    Movie default config to seperate file

--- src/config.js
+++ src/config.js
@@ -1,116 +1,6 @@
 import os from 'os'
 import fs from 'fs'
 
-let configDefault = {
-    'ffmpeg': {
-
-    },
-    'indexer': {
-        'runAtBoot': false
-    },
-    'cleaner': {
-      'runAtBoot': true
-    },
-    'mdns': {
-        'enable': false
-    },
-    'queue': {
-        'concurrency': 1
-    },
-    'tvdb': {
-        'key': '4908EBCEE2556E3D'
-    },
-    'assets': {
-        'storeWithFile': false,
-        'episodeBannerLocation': '/etc/oblecto/assets/episodeBanners/',
-        'showPosterLocation': '/etc/oblecto/assets/showPosters/',
-        'moviePosterLocation': '/etc/oblecto/assets/moviePosters/',
-        'movieFanartLocation': '/etc/oblecto/assets/movieFanart/'
-    },
-    'themoviedb': {
-        'key': 'b06b4917705eeed4e4b273d4c90fe158'
-    },
-    'database': {
-        'dialect': 'sqlite',
-        'host': 'localhost',
-        'username': 'root',
-        'password': 'root',
-        'database': 'Oblecto'
-    },
-    'server': {
-        'port': 8080
-    },
-    'tvshows': {
-        'doReIndex': false,
-        'ignoreSeriesMismatch': true,
-        'indexBroken': false,
-        'directories': [
-
-        ]
-    },
-    'movies': {
-        'doReIndex': false,
-        'indexBroken': false,
-        'directories': [
-
-        ]
-    },
-    'artwork': {
-        'fanart': {
-            'small': 100,
-            'medium': 500,
-            'large': 1000
-        },
-        'poster': {
-            'small': 100,
-            'medium': 500,
-            'large': 1000
-        },
-        'banner': {
-            'small': 100,
-            'medium': 500,
-            'large': 1000
-        }
-    },
-    'fileExtensions': {
-      'video': ['.mp4','.avi', '.iso', '.m4v', '.mkv', '.mk3d']
-    },
-    'authentication': {
-        'secret': 'secret',
-        'saltRounds': 10
-    },
-    'tracker': {
-        'interval': 10
-    },
-    'transcoding': {
-        'doRealTimeRemux': true,
-        'doRealTimeTranscode': true,
-        'transcodeEverything': false,
-        'iso': 'mp4'
-    },
-    'web': {
-        'enabled': true
-    },
-    'federation': {
-        'key': '/etc/oblecto/id_rsa',
-        'dataPort': 9131,
-        'mediaPort': 9132,
-        'servers' : {
-            'oblecto': {
-                'address': 'oblecto',
-                'ca': '/etc/oblecto/keys/oblecto.pem',
-                'dataPort': 9131,
-                'mediaPort': 9132
-            }
-        },
-        'clients': {
-            'tria': {
-                'key': '/etc/oblecto/keys/tria.pub'
-            }
-        }
-    }
-};
-
 let config = {};
 
 const ConfigManager = {
@@ -130,10 +20,7 @@ const ConfigManager = {
     },
     loadConfigFiles: function loadConfigs () {
         config = {
-            ...configDefault,
-            ...this.loadFile('/etc/oblecto/config.json'),
-            ...this.loadFile(os.homedir() + "/.oblecto.json"),
-            ...this.loadFile(__dirname + '/userconfig.json'),
+            ...this.loadFile('/etc/oblecto/config.json')
         }
     },
     saveConfig: function saveConfig () {
