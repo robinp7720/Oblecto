09498f6e0d5fdd9e9dbf40b9ad6f1bb8ff1245e1
Robin <robin@decker.cx>
2020-07-15 12:18:52 +0200

    Rename tvshow to series

--- src/submodules/REST/routes/settings/maintenance.js
+++ src/submodules/REST/routes/settings/maintenance.js
@@ -9,23 +9,23 @@ export default (server, oblecto) => {
     // API Endpoint to request a re-index of certain library types
     server.get('/settings/maintenance/index/:libraries', authMiddleWare.requiresAuth, function (req, res) {
         switch (req.params.libraries) {
-        case 'tvshows':
-            oblecto.seriesCollector.collectAll();
-            break;
-        case 'movies':
-            oblecto.movieCollector.collectAll();
-            break;
-        case 'all':
-            oblecto.seriesCollector.collectAll();
-            oblecto.movieCollector.collectAll();
-            break;
+            case 'series':
+                oblecto.seriesCollector.collectAll();
+                break;
+            case 'movies':
+                oblecto.movieCollector.collectAll();
+                break;
+            case 'all':
+                oblecto.seriesCollector.collectAll();
+                oblecto.movieCollector.collectAll();
+                break;
         }
 
         res.send([true]);
     });
 
     // API Endpoint to request a re-index of certain library types
-    server.get('/settings/maintenance/tvshows/download/art', authMiddleWare.requiresAuth, function (req, res) {
+    server.get('/settings/maintenance/series/download/art', authMiddleWare.requiresAuth, function (req, res) {
         oblecto.seriesArtworkCollector.collectAll();
         res.send([true]);
     });
@@ -33,25 +33,40 @@ export default (server, oblecto) => {
 
     server.get('/settings/maintenance/movies/download/art', authMiddleWare.requiresAuth, function (req, res) {
         oblecto.movieArtworkCollector.collectAll();
-
         res.send([true]);
     });
+
+    server.get('/settings/maintenance/update/series', authMiddleWare.requiresAuth, function (req, res) {
+        oblecto.seriesUpdateCollector.collectAllSeries();
+        res.send([true]);
+    });
+
+    server.get('/settings/maintenance/update/episodes', authMiddleWare.requiresAuth, function (req, res) {
+        oblecto.seriesUpdateCollector.collectAllEpisodes();
+        res.send([true]);
+    });
+
+    server.get('/settings/maintenance/update/movies', authMiddleWare.requiresAuth, function (req, res) {
+        oblecto.movieUpdateCollector.collectAllMovies();
+        res.send([true]);
+    });
+
 
     server.get('/settings/maintenance/clean/:type', authMiddleWare.requiresAuth, function (req, res) {
 
         switch  (req.params.type) {
-        case 'files':
+            case 'files':
 
-            break;
-        case 'movies':
+                break;
+            case 'movies':
 
-            break;
-        case 'tvshows':
+                break;
+            case 'series':
 
-            break;
-        case 'episodes':
+                break;
+            case 'episodes':
 
-            break;
+                break;
         }
 
         res.send([true]);
