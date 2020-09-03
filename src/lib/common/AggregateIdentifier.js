62927ecc3aa06ecfcdad75c8b48d434a5ece5bd7
Robin <robin@decker.cx>
2020-09-03 12:33:37 +0200

    Add type to jsdoc

--- src/lib/common/AggregateIdentifier.js
+++ src/lib/common/AggregateIdentifier.js
@@ -10,7 +10,7 @@ export default class AggregateIdentifier {
 
     /**
      *
-     * @param {} identifier
+     * @param {MediaIdentifier} identifier
      */
     loadIdentifier(identifier) {
         this.identifiers.push(identifier);
