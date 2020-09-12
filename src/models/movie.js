ec65ebeddbc3517ab4429a4dc57d1b76df16271f
Robin <robin@decker.cx>
2020-09-12 11:44:49 +0200

    Add additional movie information to database

--- src/models/movie.js
+++ src/models/movie.js
@@ -4,8 +4,18 @@ export class Movie extends Model {}
 
 export const movieColumns = {
     tmdbid: { type: DataTypes.INTEGER, allowNull: true, unique: true },
+    imdbid: { type: DataTypes.STRING, allowNull: true, unique: true },
 
     movieName: DataTypes.STRING,
+    originalName: DataTypes.STRING,
+    tagline: DataTypes.STRING,
+
+    originalLanguage: DataTypes.STRING,
+
+    budget: DataTypes.INTEGER,
+    revenue: DataTypes.INTEGER,
+
+    runtime: DataTypes.INTEGER,
 
     popularity: DataTypes.FLOAT,
 
