d001bab63d90aead965a611b310d660476fc5cdc
Robin <robin@decker.cx>
2020-09-12 11:44:49 +0200

    Add additional movie information to database

--- src/lib/updaters/movies/informationRetrievers/TmdbMovieRetriever.js
+++ src/lib/updaters/movies/informationRetrievers/TmdbMovieRetriever.js
@@ -11,7 +11,19 @@ export default class TmdbMovieRetriever {
         let movieInfo = await this.oblecto.tmdb.movieInfo({ id: movie.tmdbid });
 
         let data = {
+            imdbid: movieInfo.imdb_id,
+
             movieName: movieInfo.title,
+            originalName: movieInfo.original_title,
+            tagline: movieInfo.tagline,
+
+            originalLanguage: movieInfo.original_language,
+
+            budget: movieInfo.budget,
+            revenue: movieInfo.revenue,
+
+            runtime: movieInfo.runtime,
+
             overview: movieInfo.overview,
             popularity: movieInfo.popularity,
             releaseDate: movieInfo.release_date
