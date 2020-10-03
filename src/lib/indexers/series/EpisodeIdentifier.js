384289017fa291e10f2cf0b3ed02233d8dc92246
Robin <robin@decker.cx>
2020-10-03 21:35:11 +0200

    Add type to jsdoc

--- src/lib/indexers/series/EpisodeIdentifier.js
+++ src/lib/indexers/series/EpisodeIdentifier.js
@@ -4,7 +4,7 @@ export default class EpisodeIdentifier extends MediaIdentifier {
     /**
      * Identify an Episode based a file path and series information
      * @param {String} path File path to the Episode
-     * @param series Series object from Sequalize
+     * @param {Series} series Series object from Sequalize
      * @returns {Promise<JSON>}
      */
     async identify(path, series) {
