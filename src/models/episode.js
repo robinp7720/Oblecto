40249f41d03a9d163a695ffe072da0e3ecef6d55
Robin <robin@decker.cx>
2020-10-12 02:10:38 +0200

    Metadata columns should be nullable

--- src/models/episode.js
+++ src/models/episode.js
@@ -7,14 +7,14 @@ export const episodeColumns = {
     tmdbid: { type: DataTypes.INTEGER, allowNull: true, unique: true },
     imdbid: { type: DataTypes.STRING, allowNull: true, unique: true },
 
-    episodeName: DataTypes.STRING,
+    episodeName: { type: DataTypes.STRING, allowNull: true},
 
-    absoluteNumber: DataTypes.INTEGER,
-    airedEpisodeNumber: DataTypes.INTEGER,
-    airedSeason: DataTypes.INTEGER,
-    dvdEpisodeNumber: DataTypes.DOUBLE,
-    dvdSeason: DataTypes.INTEGER,
+    absoluteNumber: { type: DataTypes.INTEGER, allowNull: true},
+    airedEpisodeNumber: { type: DataTypes.STRING, allowNull: false},
+    airedSeason: { type: DataTypes.STRING, allowNull: false},
+    dvdEpisodeNumber: { type: DataTypes.STRING, allowNull: true},
+    dvdSeason: { type: DataTypes.STRING, allowNull: true},
 
-    firstAired: DataTypes.DATEONLY,
-    overview: DataTypes.TEXT,
+    firstAired: { type: DataTypes.DATEONLY, allowNull: true},
+    overview: { type: DataTypes.STRING, allowNull: true},
 };
