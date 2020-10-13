4a87c2c5886e15ab7fe284a38659d0b62e43d244
Robin <robin@decker.cx>
2020-10-14 00:36:26 +0200

    Add argument to alter database using --alter

--- src/bin/scripts/init/database.js
+++ src/bin/scripts/init/database.js
@@ -3,11 +3,13 @@ import {initDatabes} from '../../../submodules/database';
 export default async (args) => {
     const sequelize = initDatabes();
 
-    await sequelize.authenticate()
-        .then(() => {
-            // Create databases if connection to the database could be established
-            return sequelize.sync();
-        });
+    let options = {};
+
+    if ('--alter' in args)
+        options = {alter: true};
+
+    await sequelize.authenticate();
+    await sequelize.sync(options);
 
     await sequelize.close();
 };
