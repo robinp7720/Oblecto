62dbc4696b7267e7ba7a51faf6b75179b982148e
Hampton Moore <me@hampton.pw>
2020-10-17 18:39:23 -0400

    Alow JWT to be passed as auth query

--- src/submodules/REST/middleware/auth.js
+++ src/submodules/REST/middleware/auth.js
@@ -7,7 +7,7 @@ export default {
         if (req.authorization === undefined)
             return next(new errors.UnauthorizedError('Session is not authenticated'));
 
-        jwt.verify(req.authorization.credentials, config.authentication.secret, function (err, decoded) {
+        jwt.verify(req.authorization.credentials || req.params.auth, config.authentication.secret, function (err, decoded) {
             if (err)
                 return next(new errors.UnauthorizedError('An error has occurred during session authentication'));
 
