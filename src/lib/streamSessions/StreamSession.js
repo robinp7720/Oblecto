658b5e5ff771de68790fd77850e8bc7c88743745
Zeyphros <robin@decker.cx>
2020-12-26 18:03:40 +0100

    Cleanup video codec selection function

--- src/lib/streamSessions/StreamSession.js
+++ src/lib/streamSessions/StreamSession.js
@@ -124,17 +124,15 @@ export default class StreamSession extends events.EventEmitter{
     }
 
     getFfmpegVideoCodec() {
-        let codec = this.videoCodec;
-
         if (this.oblecto.config.transcoding.hardwareAcceleration) {
             if (this.oblecto.config.transcoding.hardwareAccelerator === 'cuda') {
-                if (codec === 'h264'){
+                if (this.videoCodec === 'h264'){
                     return 'h264_nvenc';
                 }
             }
 
             if (this.oblecto.config.transcoding.hardwareAccelerator === 'vaapi') {
-                if (codec === 'h264'){
+                if (this.videoCodec === 'h264'){
                     return 'h264_vaapi';
                 }
             }
@@ -144,8 +142,8 @@ export default class StreamSession extends events.EventEmitter{
             'h264': 'libx264'
         };
 
-        if (codecs[codec]) codec = codecs[codec];
+        if (codecs[this.videoCodec]) return codecs[this.videoCodec];
 
-        return codec;
+        return this.videoCodec;
     }
 }
