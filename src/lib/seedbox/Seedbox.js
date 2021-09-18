250a6ff738e13eb59cc006fd25b6bcd9494e1111
Zeyphros <robin@decker.cx>
2021-09-18 20:21:14 +0200

    Add method to find files recursively

--- src/lib/seedbox/Seedbox.js
+++ src/lib/seedbox/Seedbox.js
@@ -1,9 +1,13 @@
+import { extname } from 'path';
 import SeedboxImportFTP from './SeedboxImportDrivers/SeedboxImportFTP';
 import WarnExtendableError from '../errors/WarnExtendableError';
 
 export default class Seedbox {
     constructor(seedboxConfig) {
+        this.moviePath = seedboxConfig.moviePath;
+        this.seriesPath = seedboxConfig.seriesPath;
 
+        this.initStorageDriver(seedboxConfig.storageDriver, seedboxConfig.storageDriverOptions);
     }
 
     initStorageDriver(seedboxStorageDriver, seedboxStorageDriverOptions) {
@@ -14,5 +18,32 @@ export default class Seedbox {
         }
 
         return new WarnExtendableError('Invalid seedbox storage driver');
+    }
+
+    async findAll(indexPath, fileTypes) {
+        const toIndex = [];
+        const indexed = [];
+
+        toIndex.push(indexPath);
+
+        while (toIndex.length > 0) {
+            let current = toIndex.pop();
+
+            const entries = await this.storageDriver.list(current);
+
+            for (const entry of entries) {
+                switch (entry.type) {
+                    case 0: // Type 0 is a file
+                        if (fileTypes.indexOf(extname(`${current}/${entry.name}`).replace('.','')) !== -1)
+                            indexed.push(`${current}/${entry.name}`);
+                        break;
+                    case 1: // Type 1 is a directory
+                        toIndex.push(`${current}/${entry.name}`);
+                        break;
+                }
+            }
+        }
+
+        return indexed;
     }
 }
