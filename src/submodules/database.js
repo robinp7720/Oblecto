5fda0eea7c3438dd9e31a657c8be581473325e50
Zeyphros <robin@decker.cx>
2022-10-30 10:37:49 +0100

    Use tsc for building

--- src/submodules/database.js
+++ src/submodules/database.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import config from '../config.ts';
+import config from '../config';
 import logger from './logger';
 
 import { Episode, episodeColumns } from '../models/episode';
