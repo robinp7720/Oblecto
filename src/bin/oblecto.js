0a8ec6bba23edfb94f165d9af5da4f59d8af65ea
Robin Decker <robin@decker.cx>
2025-06-04 00:11:43 +0200

    Add error handling for command execution in oblecto.js
    This update wraps the command execution logic in a try-catch block to improve robustness and provide more informative error messages. Any errors encountered during execution are now logged to the console, enhancing debugging and user experience.

--- src/bin/oblecto.js
+++ src/bin/oblecto.js
@@ -5,57 +5,61 @@ import { promises as fs } from 'fs';
     let packageInfo = JSON.parse(await fs.readFile(__dirname + '/../../package.json'));
     let args = process.argv.slice(2);
 
-    switch (args[0]) {
-        case 'start':
-            require('../core/index').default.start();
-            break;
-        case 'start-tui':
-            require('../core/graphical').default.start();
-            break;
+    try {
+        switch (args[0]) {
+            case 'start':
+                require('../core/index').default.start();
+                break;
+            case 'start-tui':
+                require('../core/graphical').default.start();
+                break;
 
-        case 'init':
-            await require('./scripts/init').default(args);
+            case 'init':
+                await require('./scripts/init').default(args);
 
-            break;
+                break;
 
-        case 'adduser':
-            await require('./scripts/adduser').default(args);
+            case 'adduser':
+                await require('./scripts/adduser').default(args);
 
-            break;
+                break;
 
-        case 'changepassword':
-            await require('./scripts/changepassword').default(args);
-            break;
+            case 'changepassword':
+                await require('./scripts/changepassword').default(args);
+                break;
 
-        case 'removepassword':
-            await require('./scripts/removepassword').default(args);
-            break;
+            case 'removepassword':
+                await require('./scripts/removepassword').default(args);
+                break;
 
-        case 'deluser':
-            await require('./scripts/deluser').default(args);
+            case 'deluser':
+                await require('./scripts/deluser').default(args);
 
-            break;
+                break;
 
-        default:
-            console.log(`Oblecto ${packageInfo.version}`);
-            console.log();
-            console.log('First time setup:');
-            console.log('oblecto init');
-            console.log('oblecto init database');
-            console.log();
-            console.log('Start oblecto without TUI:');
-            console.log('oblecto start');
-            console.log('Start oblecto with TUI:');
-            console.log('oblecto start-tui');
-            console.log();
-            console.log('User maintenance:');
-            console.log('oblecto adduser USERNAME PASSWORD REALNAME EMAIL');
-            console.log('oblecto deluser USERNAME');
-            console.log('oblecto changepassword USERNAME PASSWORD');
-            console.log('oblecto removepassword USERNAME');
-            console.log();
-            console.log('Server maintenance:');
-            console.log('oblecto init assets');
+            default:
+                console.log(`Oblecto ${packageInfo.version}`);
+                console.log();
+                console.log('First time setup:');
+                console.log('oblecto init');
+                console.log('oblecto init database');
+                console.log();
+                console.log('Start oblecto without TUI:');
+                console.log('oblecto start');
+                console.log('Start oblecto with TUI:');
+                console.log('oblecto start-tui');
+                console.log();
+                console.log('User maintenance:');
+                console.log('oblecto adduser USERNAME PASSWORD REALNAME EMAIL');
+                console.log('oblecto deluser USERNAME');
+                console.log('oblecto changepassword USERNAME PASSWORD');
+                console.log('oblecto removepassword USERNAME');
+                console.log();
+                console.log('Server maintenance:');
+                console.log('oblecto init assets');
 
+        }
+    } catch (e) {
+        console.log('An error has occured: ', e);
     }
 })();
