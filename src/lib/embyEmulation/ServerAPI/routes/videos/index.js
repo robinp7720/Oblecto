e51c3eff44c48db33438b93ee30967f7f8d3f205
Zeyphros <robin@decker.cx>
2025-06-15 19:05:43 +0200

    - Migrate from Restify to Express for server implementation (#135)
    - Replace `restify-errors` with custom error module
    - Update routes and middleware compatibility with Express
    - Refactor responses and add support for log and error handling
    - Remove unused collections and refactor data structure sending to clients

--- src/lib/embyEmulation/ServerAPI/routes/videos/index.js
+++ src/lib/embyEmulation/ServerAPI/routes/videos/index.js
@@ -1,11 +1,11 @@
 import { Movie } from '../../../../../models/movie';
 import { File } from '../../../../../models/file';
 import { Stream } from '../../../../../models/stream';
-import errors from 'restify-errors';
+import errors from '../../errors';
 import DirectHttpStreamSession from '../../../../streamSessions/StreamSessionTypes/DirectHttpStreamSession';
 
 export default (server, embyEmulation) => {
-    server.get('/videos/:mediaid/stream.:ext', async (req, res) => {
+    server.get('/videos/:mediaid/stream/:ext', async (req, res) => {
         // if (!embyEmulation.oblecto.streamSessionController.sessionExists(req.params.mediasourceid)) {
         //    console.log('The stream session doesn\'t exist');
         //    return new errors.InvalidCredentialsError('Stream session token does not exist');
