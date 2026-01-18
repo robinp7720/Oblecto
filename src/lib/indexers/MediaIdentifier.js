96fb84456deec1ac2008bfda18c6b7d21dd78414
Robin Decker <robin@decker.cx>
2026-01-18 15:08:39 +0100

    TS Migration: Convert `MediaIdentifier.js` to TypeScript

--- src/lib/indexers/MediaIdentifier.js
+++
@@ -1,5 +0,0 @@
-export default class MediaIdentifier {
-    constructor(oblecto) {
-        this.oblecto = oblecto;
-    }
-}
---
+++ src/lib/indexers/MediaIdentifier.ts
@@ -0,0 +1,9 @@
+import Oblecto from '../oblecto/index.js';
+
+export default class MediaIdentifier {
+    public oblecto: Oblecto;
+
+    constructor(oblecto: Oblecto) {
+        this.oblecto = oblecto;
+    }
+}
