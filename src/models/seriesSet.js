91381fe5928c9fc2c417b21cc75dfc066fc108f0
Robin Decker <robin@decker.cx>
2026-01-18 14:37:04 +0100

    Add `tmdbid` and `public` fields to `seriesSet` model
    - Added nullable, unique `tmdbid` field for external ID tracking.
    - Introduced `public` field with a default value of `true` for visibility control.

--- src/models/seriesSet.js
+++ src/models/seriesSet.js
@@ -5,4 +5,8 @@ export class SeriesSet extends Model {}
 export const seriesSetColumns = {
     setName: DataTypes.STRING,
     overview: DataTypes.TEXT,
+    tmdbid: {
+        type: DataTypes.INTEGER, allowNull: true, unique: true 
+    },
+    public: { type: DataTypes.BOOLEAN, defaultValue: true },
 };
